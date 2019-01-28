1ac1e1989573385ffe3e9bb9a3a0a1fb09e275e3
feuken <37409987+feuken@users.noreply.github.com>
2019-01-28 10:19:47 +0100

    abc

---
+++ ShowroomBooking - Copy/ShowroomBooking/Models/Dag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShowroomBooking.Models
+{
+    public class Dag
+    {   
+        
+        public DateTime Datum { get; set; }
+        public virtual List<Tid> Tider { get; set; }
+    }
+}
---
+++ ShowroomBooking - Copy/ShowroomBooking/Models/Events.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShowroomBooking.Models
+{
+    public class Events
+    {
+        public DateTime EventStart { get; set; }
+        public DateTime EventEnd { get; set; }
+    }
+}
